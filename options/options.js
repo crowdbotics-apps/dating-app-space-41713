29a0ef0e00217501919e4d61ffbc6ad9dbac986b
Crowdbotics <team@crowdbotics.com>
2023-06-24 04:07:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dating-app-space-41713",
+  url: "https://dating-app-space-41713.botics.co",
+  api: "https://dating-app-space-41713.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
